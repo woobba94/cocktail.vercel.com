0dd8bae710863be854357a9c6492f38889ea11e8
woobba94 <jwj3199@naver.com>
2022-03-05 01:54:35 +0900

    ✏️ Typo : remove dead code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import type { AppProps } from 'next/app';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <h1>안녕하세요</h1>
       <Component {...pageProps} />
     </>
   );
