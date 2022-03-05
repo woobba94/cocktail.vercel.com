970b96f540e3727406043b85cf2b742438762707
jiseung-kang <ill0o0lli@ajou.ac.kr>
2022-03-05 17:35:27 +0900

    💩 Test : load multiple api

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,19 +1,40 @@
+import { useRef, useState } from 'react';
+
 interface Props {
-  setValue: (value: string) => void;
+  // setSelectedItems: (value: [] | string[]) => void;
+  selectedItems: string[];
+  setIsSubmitted: (value: boolean) => void;
 }
 
-const SearchBar = ({ setValue }: Props) => {
+const SearchBar = ({ selectedItems, setIsSubmitted }: Props) => {
+  const [inputValue, setInputValue] = useState('');
+  const count = useRef(0);
+
   const handleOnChange = (e: any) => {
-    setValue(e.target.value);
+    setInputValue(e.target.value);
   };
   const handleOnClick = () => {
     // 다중 재료 추가
+    if (inputValue !== '' && count.current < 3) {
+      const newArr: string[] = [...selectedItems];
+      newArr[count.current] = inputValue;
+      selectedItems = newArr;
+      // setSelectedItems(newArr);
+      count.current += 1;
+      console.log(selectedItems);
+    }
+  };
+
+  const handleOnSubmit = () => {
+    // 조회
+    setIsSubmitted(true);
   };
 
   return (
     <div>
       <input placeholder="재료 입력" onChange={handleOnChange} />
-      <button onClick={handleOnClick}>제출</button>
+      <button onClick={handleOnClick}>추가</button>
+      <button onClick={handleOnSubmit}>제출</button>
     </div>
   );
 };
