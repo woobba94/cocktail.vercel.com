73fc65f6a4bacc1c463165c611f30ebccba0306b
jiseung-kang <ill0o0lli@ajou.ac.kr>
2022-03-16 00:17:36 +0900

    💄 Update: Add result box styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,18 @@
 import type { AppProps } from 'next/app';
+import styled from 'styled-components';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <App>
       <div>칵테일 서비스</div>
       <Component {...pageProps} />
-    </>
+    </App>
   );
 }
+
+const App = styled.div`
+  position: relative;
+  display: block;
+`;
 
 export default MyApp;
