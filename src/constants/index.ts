1f2e9a5221dcfe73e3955c1f2d511a7e3e279d7e
jiseung-kang <ill0o0lli@ajou.ac.kr>
2022-03-17 19:06:42 +0900

    ✨ Feat: Add searching category component

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,14 @@
 export const API_ENDPOINT = 'https://www.thecocktaildb.com/api/json/v1/1/';
 export const API_ENDPOINT_2 = 'https://www.thecocktaildb.com/api/json/v2/1/';
+
+export const CATEGORY = [
+  'absolut',
+  'ale',
+  'brandy',
+  'whiskey',
+  'rum',
+  'bourbon',
+  'applejack',
+  'champagne',
+  'cognac',
+];
