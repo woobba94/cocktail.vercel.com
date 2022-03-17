0b9b1f8519f83d2ddfaadc3e6be008423f0958dd
jiseung-kang <ill0o0lli@ajou.ac.kr>
2022-03-17 19:09:44 +0900

    ♻️ Refactor: Load API once in parent component
    - 데이터 로드를 부모 컴포넌트에서 한번만 실행
    - 부모 컴포넌트에서 데이터 가공 후 props로 전달

---
+++ src/components/SearchContainer.tsx
@@ -0,0 +1,76 @@
+import { useEffect, useRef, useState } from 'react';
+import styled from 'styled-components';
+import AutoComplete from './AutoComplete';
+import { useData } from 'src/hooks/useData';
+import MenuTab from './Category';
+
+interface SearchContainerProps {
+  selectedList: string[];
+  setSelectedList: (value: string[]) => void;
+  setIsSubmitted: (value: boolean) => void;
+}
+
+const SearchContainer = ({
+  selectedList,
+  setSelectedList,
+  setIsSubmitted,
+}: SearchContainerProps) => {
+  const [inputValue, setInputValue] = useState<string>('');
+  const ingredients = useRef<string[]>([]);
+
+  const { data, error } = useData('list.php?i=list', '');
+
+  if (data && ingredients.current.length === 0) {
+    Object.values(data.drinks).forEach((ingredient: any) => {
+      ingredients.current.push(ingredient.strIngredient1);
+    });
+    ingredients.current.sort();
+  }
+
+  useEffect(() => {
+    setIsSubmitted(false);
+  }, [selectedList]);
+
+  const handleOnChange = (e: any) => {
+    setInputValue(e.target.value);
+  };
+  const handleOnAdd = () => {
+    if (inputValue !== '' && selectedList.includes(inputValue) == false) {
+      const newArr = [...selectedList];
+      newArr.push(inputValue);
+      setSelectedList(newArr);
+    }
+    setInputValue('');
+  };
+
+  const handleOnSubmit = () => {
+    setIsSubmitted(true);
+  };
+
+  return (
+    <Container>
+      <input
+        placeholder="재료 입력"
+        onChange={handleOnChange}
+        value={inputValue}
+      />
+      {ingredients.current !== [] ? (
+        <AutoComplete
+          inputValue={inputValue}
+          ingredients={ingredients.current}
+        />
+      ) : (
+        <></>
+      )}
+      <MenuTab ingredients={ingredients.current} />
+      <button onClick={handleOnAdd}>추가</button>
+      <button onClick={handleOnSubmit}>제출</button>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  background-color: whitesmoke;
+`;
+
+export default SearchContainer;
