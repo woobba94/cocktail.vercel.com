1711d562c071c3057eba77a0fa6c77b7d9cb29e2
woobba94 <jwj3199@naver.com>
2022-03-25 22:12:03 +0900

    ♻️ Refactor: AutoComplete component
    - 중복코드 정리
    - 추상화

--- src/components/AutoComplete.tsx
+++ src/components/AutoComplete.tsx
@@ -5,7 +5,7 @@ interface AutoCompleteProps {
   ingredients: string[];
   handleOnAdd: (value: any) => void;
   currentFocus: number;
-  setCurrentFocus: (value: any) => void;
+  setCurrentFocus: (value: number) => void;
   setInputValue: (value: string) => void;
   isNewInput: boolean;
   setCurrentItem: (value: string) => void;
@@ -26,73 +26,74 @@ const AutoComplete = ({
   const [searchedIngredients, setSearchedIngredients] = useState<string[]>([]);
 
   useEffect(() => {
-    if (currentFocus >= searchedIngredients.length) {
-      setCurrentFocus(0);
-    } else if (currentFocus < 0) {
-      setCurrentFocus(searchedIngredients.length - 1);
+    // 포커스가 넘어갈 때 포커스 컨트롤
+    if (currentFocus < 0 || currentFocus === searchedIngredients.length) {
+      currentFocus < 0
+        ? setCurrentFocus(searchedIngredients.length - 1)
+        : setCurrentFocus(0);
     }
 
-    if (searchedIngredients[currentFocus]) {
-      const ingredient = searchedIngredients[currentFocus]
-        .replaceAll('<b>', '')
-        .replaceAll('</b>', '');
-      if (isArrowPressed) setInputValue(ingredient);
-    }
+    // null인 경우 return => 오버플로우가 일어남
+    if (!searchedIngredients[currentFocus]) return;
+    const ingredient = removeHighlight(searchedIngredients[currentFocus]);
+    setCurrentItem(ingredient);
 
-    setCurrentItem(
-      searchedIngredients[currentFocus]
-        ?.replaceAll('<b>', '')
-        .replaceAll('</b>', ''),
-    );
+    // 포커스에 따라 input값 세팅
+    isArrowPressed && setInputValue(ingredient);
   }, [currentFocus]);
 
   useEffect(() => {
     setCurrentFocus(0);
-    setCurrentItem(
-      searchedIngredients[currentFocus]
-        ?.replaceAll('<b>', '')
-        .replaceAll('</b>', ''),
-    );
+    // null인 경우 return => 오버플로우가 일어남
+    if (!searchedIngredients[currentFocus]) return;
+    const ingredient = removeHighlight(searchedIngredients[currentFocus]);
+    setCurrentItem(ingredient);
   }, [searchedIngredients]);
 
   useEffect(() => {
-    if (!inputValue) {
-      setSearchedIngredients([]);
-    } else if (isNewInput && ingredients) {
-      ingredients.sort();
-      const newArr: string[] = [];
-
-      ingredients.forEach((ingredient: string) => {
-        ingredient = ingredient.toLowerCase();
-        inputValue = inputValue.toLowerCase();
-
-        const index = ingredient.indexOf(inputValue);
-
-        // 일치할 때
-        if (index > -1) {
-          ingredient = ingredient.replaceAll(
-            inputValue,
-            `<b>${inputValue}</b>`,
-          );
-
-          // 첫번째 문자가 일치
-          if (index === 0) {
-            ingredient = ingredient.replaceAll(
-              inputValue,
-              // 대문자로 변경
-              inputValue.charAt(0).toUpperCase() + inputValue.slice(1),
-            );
-          } else {
-            ingredient =
-              ingredient.charAt(0).toUpperCase() + ingredient.slice(1);
-          }
-
-          newArr.push(ingredient);
-        }
-      });
-      setSearchedIngredients(newArr);
+    // 새로운값이 아니거나 빈문자열
+    if (!isNewInput || !inputValue) {
+      !inputValue && setSearchedIngredients([]);
+      return;
     }
+    const newArr: string[] = [];
+
+    ingredients.sort();
+    ingredients.forEach((ingredient: string) => {
+      ingredient = ingredient.toLowerCase();
+      inputValue = inputValue.toLowerCase();
+      const index = ingredient.indexOf(inputValue);
+      // 일치하는 문자가 없으면 return
+      if (index === -1) return;
+
+      ingredient = addHighlight(ingredient);
+
+      // 첫번째 문자부터 일치하면 inputValue 첫글자를 대문자로
+      // 아니면 ingredient 첫글자 변경
+      ingredient =
+        index === 0
+          ? ingredient.replaceAll(inputValue, capitalize(inputValue))
+          : capitalize(ingredient);
+
+      newArr.push(ingredient);
+    });
+
+    setSearchedIngredients(newArr);
   }, [inputValue]);
+
+  // 첫글자 대문자로 반환
+  const capitalize = (str: string) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  };
+
+  // 문자열 강조
+  const addHighlight = (str: string) => {
+    return str.replaceAll(inputValue, `<b>${inputValue}</b>`);
+  };
+  // 문자열 일반화
+  const removeHighlight = (str: string) => {
+    return str.replaceAll('<b>', '').replaceAll('</b>', '');
+  };
 
   return (
     <Container>
