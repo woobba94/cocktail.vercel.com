566cda3840edc7f705c0c363e2e0a9c23c412b02
woobba94 <jwj3199@naver.com>
2022-03-27 00:45:37 +0900

    ✨ Feat: Favorites page(prototype)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,14 @@
+import Link from 'next/link';
 import styled from 'styled-components';
-
 const Header = () => {
+  const handleOnStar = () => {
+    console.log('즐겨찾기 클릭');
+  };
+
   return (
     <HeaderContainer>
       <div>로고</div>
-      <div>즐겨찾기</div>
+      <Link href={`/favorites`}>즐겨찾기</Link>
     </HeaderContainer>
   );
 };
---
+++ src/pages/favorites.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next';
+
+const Favorites: NextPage = () => {
+  return <div>즐겨찾기 목록</div>;
+};
+
+export default Favorites;
